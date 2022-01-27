d4d30d21131a842d17935d73aa70e0065ea5ad95
Elecbound <elecboy2@hotmail.com>
2022-01-27 02:19:48 -0500

    Update server.py
    Man this was annoying, also if I need to use an authenticator password every time, I'd rather just copy paste into the github. There's a better way and I have yet to care enough to find it. Okay I thought of 3 hacks, but I'm pretty sure there's a "proper way."

--- final_project/server.py
+++ final_project/server.py
@@ -7,18 +7,18 @@ app = Flask("Web Translator")
 @app.route("/englishToFrench")
 def englishToFrench():
     textToTranslate = request.args.get('textToTranslate')
-    translator.englishToFrench(textToTranslate)
+    translated_text = translator.englishToFrench(textToTranslate)
     return translated_text
 
 @app.route("/frenchToEnglish")
 def frenchToEnglish():
     textToTranslate = request.args.get('textToTranslate')
-    translator.frenchToEnglish(textToTranslate)
-    return "Translated text to English"
+    translated_text = translator.frenchToEnglish(textToTranslate)
+    return translated_text
 
 @app.route("/")
 def renderIndexPage():
-    return render_template = 'index.html'
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
